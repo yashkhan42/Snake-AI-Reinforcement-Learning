a04bc067549076d700c6785c05110a1a20969fa2
Yash Khan <tashfiqkhanyash@gmail.com>
2025-06-13 12:15:52 -0500

    finish making game

--- snake_game_human.py
+++ snake_game_human.py
@@ -121,3 +121,18 @@ class SnakeGame:
             y -= BLOCK_SIZE
             
         self.head = Point(x, y)
+        gi
+if __name__ == '__main__':
+    game = SnakeGame()
+    
+    # game loop
+    while True:
+        game_over, score = game.play_step()
+        
+        if game_over == True:
+            break
+        
+    print('Final Score', score)
+        
+        
+    pygame.quit()
