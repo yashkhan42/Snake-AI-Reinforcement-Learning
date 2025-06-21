27745f50e411e35e285a2ddda6eec8d9ad0878d2
Yash Khan <tashfiqkhanyash@gmail.com>
2025-06-21 11:50:52 -0500

    start working on agent

---
+++ agent.py
@@ -0,0 +1,64 @@
+import torch
+import random
+import numpy as np
+from collections import deque
+from game import SnakeGameAI, Direction, Point
+from model import Linear_QNet, QTrainer
+from helper import plot
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+class Agent:
+
+    def __init__(self):
+        self.n_games = 0
+        self.epsilon = 0 
+        self.gamma = 0.9 
+        self.memory = deque(maxlen=MAX_MEMORY) 
+        self.model = Linear_QNet(11, 256, 3)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+
+    def get_state(self, game):
+        head = game.snake[0]
+        point_l = Point(head.x - 20, head.y)
+        point_r = Point(head.x + 20, head.y)
+        point_u = Point(head.x, head.y - 20)
+        point_d = Point(head.x, head.y + 20)
+        
+        dir_l = game.direction == Direction.LEFT
+        dir_r = game.direction == Direction.RIGHT
+        dir_u = game.direction == Direction.UP
+        dir_d = game.direction == Direction.DOWN
+
+        state = [
+            (dir_r and game.is_collision(point_r)) or 
+            (dir_l and game.is_collision(point_l)) or 
+            (dir_u and game.is_collision(point_u)) or 
+            (dir_d and game.is_collision(point_d)),
+
+            (dir_u and game.is_collision(point_r)) or 
+            (dir_d and game.is_collision(point_l)) or 
+            (dir_l and game.is_collision(point_u)) or 
+            (dir_r and game.is_collision(point_d)),
+
+            (dir_d and game.is_collision(point_r)) or 
+            (dir_u and game.is_collision(point_l)) or 
+            (dir_r and game.is_collision(point_u)) or 
+            (dir_l and game.is_collision(point_d)),
+            
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
+            
+            game.food.x < game.head.x,  
+            game.food.x > game.head.x,  
+            game.food.y < game.head.y,  
+            game.food.y > game.head.y  
+            ]
+
+        return np.array(state, dtype=int)
+
+    
